5fc7b5177445917ccb77a0078d3df2a79282a984
Батаева Елена Валентиновна <EBataeva@stream.ru>
2021-02-28 20:31:27 +0300

    чтобы было быстрее, в первом  методе для счетчика передаю длину массива как size

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ public class Main {
 
         long t1 = System.currentTimeMillis();
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < SIZE; i++) {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
         System.out.println(System.currentTimeMillis() - t1);
